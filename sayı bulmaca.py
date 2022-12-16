1a7bc537d4f359d4cfeff96c236c2a656bc1325f
Basim Alazzawi <48827521+wthez0@users.noreply.github.com>
2022-12-16 20:17:13 +0300

    Update sayı bulmaca.py
    eksik olan yerleri düzelttim ve dahada geliştirdim

--- sayı bulmaca.py
+++ sayı bulmaca.py
@@ -1,35 +1,35 @@
 import random
 
-sayi=random.randint(1,20)
+sayi=random.randint(1,1)
 
 print("Merhaba Oyunumuza Hoşgeldiniz !")
 print("Şimdi İçimden 1 İle 20 Arasında Bir Sayı Tutacağım.")
 print("Sende Bu Sayıyı Tahmin Ediceksin !")
 print("Sadece 3 Hakkın Var Unutma.")
-print("Hazırsan Başlıyorum")
-tahmin=int(input("Lütfen Bir Tahminde Bulununuz : "))
+hazirlik=input("Hazır Mısın? E/H ")
 
-if tahmin==sayi:
-    print("Tebrikler Doğru Tahminde Bulundunuz")
-else:
-    print("Maelesef Doğru Değil Geriye Kalan Hakkınız : 2")
+while hazirlik == "E":
+    tahmin=int(input("Lütfen Bir Tahminde Bulunuz : "))
+    if tahmin == sayi:
+        print("Tahmininiz Doğru Tebrikler")
+        break
+    elif tahmin!=sayi:
+        print("Tahmininiz Yalnış Geriye Kalan Hakkınız : 2")
 
-tahmin2=int(input("Lüthen Bir Tahminde Bulununuz : "))
-if tahmin2==sayi:
-    print("Tebrikler Doğru Tahminde Bulundunuz")
-elif tahmin2!=sayi:
-    print("Maelesef Doğru Değil Geriye Kalan Hakkınız : 1")
-
-tahmin3=int(input("Lüthen Bir Tahminde Bulununuz : "))
-if tahmin3==sayi:
-    print("Tebrikler Doğru Tahminde Bulundunuz")
-elif tahmin3!=sayi:
-    print("Maelesef Doğru Değil Tuttuğum Sayı", sayi)
-
-
-
-
-
-
-
-
+    tahmin2=int(input("Lütfen Bir Tahminde Bulunuz : "))
+    if tahmin2 == sayi:
+        print("Tahmininiz Doğru Tebrikler")
+        break
+    elif tahmin2!=sayi:
+        print("Tahmininiz Yalnış Geriye Kalan Hakkınız : 1")
+        if tahmin2<sayi:
+            print("Lütfen Daha Büyük Bir Değer Giriniz.")
+        elif tahmin>sayi:
+            print("Lütfen Daha Küçük Bir Değer Giriniz")
+    
+    tahmin3=int(input("Lütfen Bir Tahminde Bulunuz : "))
+    if tahmin3 == sayi:
+        print("Tahmininiz Doğru Tebrikler")
+        break
+    elif tahmin3!=sayi:
+        print("Tahmileriniz Doğru Değil Tuttuğum Sayı :", sayi)
