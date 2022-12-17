1d069a465922fa9768a063433fedd33e6e5c9f28
Basim Alazzawi <48827521+wthez0@users.noreply.github.com>
2022-12-17 12:14:14 +0300

    Update sayı bulmaca.py

--- sayı bulmaca.py
+++ sayı bulmaca.py
@@ -1,3 +1,4 @@
+from colorama import Fore, Back, Style
 import random
 
 sayi=random.randint(1,20)
@@ -8,28 +9,36 @@ print("Sende Bu Sayıyı Tahmin Ediceksin !")
 print("Sadece 3 Hakkın Var Unutma.")
 hazirlik=input("Hazır Mısın? E/H ")
 
-while hazirlik == "E":
-    tahmin=int(input("Lütfen Bir Tahminde Bulunuz : "))
+while hazirlik == "E" and "e":
+    tahmin=int(input(Fore.CYAN +("Lütfen Bir Tahminde Bulunuz : ")))
     if tahmin == sayi:
-        print("Tahmininiz Doğru Tebrikler")
+        print(Fore.GREEN +"Tahmininiz Doğru Tebrikler")
+        print(Style.RESET_ALL)
         break
     elif tahmin!=sayi:
-        print("Tahmininiz Yalnış Geriye Kalan Hakkınız : 2")
+        print(Fore.RED + "Tahmininiz Yalnış Geriye Kalan Hakkınız : 2")
 
-    tahmin2=int(input("Lütfen Bir Tahminde Bulunuz : "))
+    tahmin2=int(input(Fore.CYAN +"Lütfen Bir Tahminde Bulunuz : "))
     if tahmin2 == sayi:
-        print("Tahmininiz Doğru Tebrikler")
+        print(Fore.GREEN +"Tahmininiz Doğru Tebrikler")
+        print(Style.RESET_ALL)
         break
     elif tahmin2!=sayi:
-        print("Tahmininiz Yalnış Geriye Kalan Hakkınız : 1")
+        print(Fore.RED +"Tahmininiz Yalnış Geriye Kalan Hakkınız : 1")
         if tahmin2<sayi:
-            print("Lütfen Daha Büyük Bir Değer Giriniz.")
+            print(Fore.YELLOW +"Lütfen Daha Büyük Bir Değer Giriniz.")
         elif tahmin>sayi:
-            print("Lütfen Daha Küçük Bir Değer Giriniz")
+            print(Fore.YELLOW +"Lütfen Daha Küçük Bir Değer Giriniz")
     
-    tahmin3=int(input("Lütfen Bir Tahminde Bulunuz : "))
+    tahmin3=int(input(Fore.CYAN +"Lütfen Bir Tahminde Bulunuz : "))
     if tahmin3 == sayi:
-        print("Tahmininiz Doğru Tebrikler")
+        print(Fore.GREEN +"Tahmininiz Doğru Tebrikler")
+        print(Style.RESET_ALL)
         break
     elif tahmin3!=sayi:
-        print("Tahmileriniz Doğru Değil Tuttuğum Sayı :", sayi)
+        print(Fore.LIGHTMAGENTA_EX +"Tahmileriniz Doğru Değil Tuttuğum Sayı :", sayi)
+        print(Style.RESET_ALL)
+else:
+    print(Fore.Yellow)
+    print(Back.RED +"Oyunu Oynamamanıza Üzüldük Tekrar Bekleriz")
+    print(Style.RESET_ALL)
